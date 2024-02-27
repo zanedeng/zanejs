5e3b18c90213c2bfb0db966e42ba9f9a58378052
dengzongheng <zongheng@fintec.ai>
2024-02-27 11:23:12 +0800

    feat: add appLayout package

---
+++ packages/appLayout/src/index.ts
@@ -0,0 +1,7 @@
+import { withInstall } from './utils'
+import appLayout from './AppLayout.vue.vue'
+
+export const AppLayout = withInstall(appLayout)
+export default AppLayout
+
+export * from './types/props'
---
+++ packages/appLayout/src/types/props.ts
@@ -0,0 +1,5 @@
+import type { CSSProperties, ExtractPropTypes, PropType } from 'vue'
+
+export const appLayoutProps = {} as const
+
+export type AppLayoutProps = ExtractPropTypes<typeof appLayoutProps>
