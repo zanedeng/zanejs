94b440661bce8309091c9f503947f9faeeead5a2
dengzongheng <zongheng@fintec.ai>
2024-02-28 23:49:43 +0800

    feat: add app example

---
+++ apps/example/src/App.vue
@@ -0,0 +1,14 @@
+<!-- eslint-disable no-irregular-whitespace -->
+<!-- eslint-disable no-irregular-whitespace -->
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+// import type { Language } from '@google-translate-select/vue3'
+const handleGoogleTranslateSelect = (language: any) => {
+  console.log(language)
+}
+</script>
+
+<template>
+  <div />
+</template>
